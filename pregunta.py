6a99b396308bf218c9b305bdf84ee8e297c8cb97
Shanic0120 <juazapataca@unal.edu.co>
2022-10-27 10:59:42 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -9,13 +9,32 @@ espacio entre palabra y palabra.
 
 
 """
+import re
 import pandas as pd
-
+import numpy as np
 
 def ingest_data():
+    archivo=pd.read_fwf(
+    "clusters_report.txt",
+    colspecs="infer",
+    widths=[8,10,20,100])
+    datos=archivo.drop([0,1],axis=0)
+    datos = pd.DataFrame(datos)
+    datos.columns=['cluster','cantidad_de_palabras_clave','porcentaje_de_palabras_clave','principales_palabras_clave']
+    newdf=pd.DataFrame(columns=['cluster','cantidad_de_palabras_clave','porcentaje_de_palabras_clave','principales_palabras_clave'])
+    c=0
+    for i in range(len(datos)):
+        if not isinstance(datos.iloc[i]['cluster'],float): 
+            newdf.loc[c]=datos.iloc[i]
+            c+=1
+        else:      
+            newdf.iloc[c-1]['principales_palabras_clave']+=" "+datos.iloc[i]['principales_palabras_clave']
 
-    #
-    # Inserte su código aquí
-    #
+    newdf['cantidad_de_palabras_clave'] = newdf['cantidad_de_palabras_clave'].astype('int')       
+    newdf['cantidad_de_palabras_clave'] = newdf['cantidad_de_palabras_clave'].astype('int')
+    newdf['porcentaje_de_palabras_clave']=newdf['porcentaje_de_palabras_clave'].apply(lambda i:float(i.split(',')[0])+float('0.'+i.split(',')[1][0]))
+    newdf['principales_palabras_clave']=newdf['principales_palabras_clave'].apply(lambda x:x[:-1] if x[-1]=='.' else x)
+    newdf['principales_palabras_clave']=newdf['principales_palabras_clave'].apply(lambda x:x.replace(',',', '))
+    newdf['principales_palabras_clave']=newdf['principales_palabras_clave'].apply(lambda x:re.sub('\s+',' ',x))
 
-    return df
+    return newdf
